48f22b2b04b4d20a6985ec48ec6706e2b1e28457
QinG1029 <134704596+QinG1029@users.noreply.github.com>
2025-06-10 15:04:00 +0800

    Update pretrain_single_gpu.sh

--- scripts/pretrain_single_gpu.sh
+++ scripts/pretrain_single_gpu.sh
@@ -13,5 +13,5 @@ fi
 #CUDA_VISIBLE_DEVICES=0,1,2,3 \
 CUDA_VISIBLE_DEVICES=0 \
 python examples/source_pretrain.py -ds ${SOURCE} -dt ${TARGET} -a ${ARCH} --seed ${SEED} --margin 0.0 \
-	--num-instances 4 -b 32 -j 4 --warmup-step 10 --lr 0.00035 --milestones 40 70 --iters 200 --epochs 120 --eval-step 1 \
+	--num-instances 4 -b 32 -j 4 --warmup-step 10 --lr 0.00035 --milestones 40 70 --iters 100 --epochs 120 --eval-step 40 \
 	--logs-dir logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-${SEED}
